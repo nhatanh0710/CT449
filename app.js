4ec50f91734430b6dfac70f3f0a64994c7d3f77b
Nhat Anh <nhatanh2629@example.com>
2025-09-30 23:56:18 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -10,4 +10,16 @@ app.use("/api/contacts", contactRouter);
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to Contact Book application' });
 });
+
+//Middleware handle 404
+app.use((req, res, next) => {
+    return next(new AppError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
  module.exports = app;       
