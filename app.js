0a54f5fb31add033edc348c46af0016beb29a746
Nhat Anh <nhatanh2629@example.com>
2025-09-30 23:48:09 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-
+const contactRouter = require('./app/routes/contact.route');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactRouter);
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to Contact Book application' });
